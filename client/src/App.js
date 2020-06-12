f266d45ec50c6e3bcaea62cdefc71b76807d587b
bhamann-collab <brockohamann@gmail.com>
2020-06-12 14:18:50 +0930

    We are now getting data from the express server to the react server

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './css/App.scss';
+//test line below
+import APITest from './component/APITest'
 import Timer from './component/Timer'
 import SwiperCard from './component/SwiperCard'
-import photoDb from './db/photoDB'
+import photoDb from './db/photoDb'
 
 console.log(photoDb)
 
 function App() {
     return (
         <div className="App">
+            <APITest />
             <Timer />
             <div className="swipeArea">
 				{photoDb.map((card) =>
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path');
 const testAPIRouter = require("./routes/testAPI")
+const cors = require("cors")
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -8,6 +9,7 @@ const port = process.env.PORT || 5000
 //Middleware
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(cors())
 
 //Using the test route
 app.use(testAPIRouter)
