8b75095c0d71dd85fbd0403ae33151f25bcc2bca
bhamann-collab <brockohamann@gmail.com>
2020-06-12 16:47:56 +0930

    Another test for heroku

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const port = process.env.PORT || 5000
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname, './client/build')))
+
+app.get('*', function(_, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'), function(err) {
+        if(err) {
+            res.status(500).send(err)
+        }
+    })
+})
 
 //Using the test route
 app.use(testAPIRouter)
