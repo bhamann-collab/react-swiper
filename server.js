a7c350bb630cb38b5828282e3d2ff9adb4438011
bhamann-collab <brockohamann@gmail.com>
2020-06-12 23:37:56 +0930

    404 error heroku

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const port = process.env.PORT || 5000
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
-app.use(express.static(path.join(__dirname, './client/public')))
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/public"))
+}
+app.use(express.static(path.join(__dirname, 'client/public')))
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'index.html'))
