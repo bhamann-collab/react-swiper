7c143dcc978286454504a91b591211ce10c8e68b
bhamann-collab <brockohamann@gmail.com>
2020-06-12 23:04:14 +0930

    fixing 404 error heroku

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const port = process.env.PORT || 5000
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, './client/public')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 
 //Using the test route
